30817e443b80143aaf89d5a5a8b3887813a434b5
Robert Kowalski <rok@kowalski.gd>
2017-02-17 17:46:57 +0000

    is it a plane? nooo! is it a bird? noo! its superfurbie!

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# super furbie
+# super furbie!
+
+ - package.json dependencies are solely reserved for light-modules
+ - deps must have a valid index entry point, even if just an empty file
+ - style loading annoying but not unusual, maybe convention on filename (e.g. style.css?)
---
+++ _dev/app.js
@@ -0,0 +1,8 @@
+import mgnlCustom from 'mgnl-custom'
+
+import css from 'robertkowalski-shareable-component/webresources/css/shareable-component.css'
+// rename to styles.css?
+
+
+// no entry point defined in module, fails with 0.0.1
+// import rockoShareableComponent from 'robertkowalski-shareable-component'
